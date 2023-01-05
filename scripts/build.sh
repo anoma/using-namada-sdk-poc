ccdb7b4ea9ad6a1ab2ce130ca903c1230b64813b
Memas Deligeorgakis <deligeorgakismemas@googlemail.com>
2023-01-05 08:05:32 +0100

    restructuring project

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,5 +1,8 @@
-pwd
-wasm-pack build --target web
-# cp -a ./pkg/. ./distributable_wasm
-cp -a ./pkg/. ./web_app_using_namada_sdk/src/utils/namadaSdk
+# build Rust code and generate the glue code
+(cd usage-of-namada-sdk; wasm-pack build --target web)
+
+# copy the resulting artifacts to the web app project
+cp -a ./usage-of-namada-sdk/pkg/. ./web_app_using_namada_sdk/src/utils/namadaSdk
+
+# copy also the JavaScript dependencies
 cp -a ./javascript_dependencies/networkingUtils.ts ./web_app_using_namada_sdk/src/utils/namadaSdk
